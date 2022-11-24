b013c98af1caf3b4b7f6e50099402bd8b3952280
Adewoyin Aanuoluwapo <adewoyinaanuoluwapo@gmail.com>
2022-11-24 21:57:29 +0100

    feat: footer styling and responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,34 @@
-import React from 'react'
+import PlaystoreIcon from "../components/assets/google-play.png";
+import AppleIcon from "../components/assets/app-store.png";
+import CompanyLogo from './assets/CompanyLogo.png'
+import '../styles/Footer.css'
+
 
 export default function Footer() {
   return (
       <footer>
-          <div className="">
-        <div className="">
-          <div>
-            <img src="logo-white.png" alt="logo" />
+          <div className="footer">
+        <div className="first-section">
+          <div className="footer__nav">
+            <img src={CompanyLogo} className="footer__logo" alt="logo" />
             <div className="footer__links">
-              <a href="/" className="footer__link">About us</a>
+              <a href="/" className="footer__link">Culture</a>
               <a href="/" className="footer__link">Careers</a>
               <a href="/" className="footer__link">Weather News</a>
               <a href="/" className="footer__link">For Business</a>
               <a href="/" className="footer__link">FAQs</a>
               <a href="/" className="footer__link">Contact Us</a>
-              <a href="/" className="footer__link">Culture</a>
             </div>
           </div>
-          <div>
-            <span className="footer-title">Download the mobile app</span>
-            <div className="">
-              <img src="app-store.png" alt="app store" />
-              <img src="google-play.png" alt="google play" />
+          <div className="footer__app">
+            <span className="footer__app-title">Download the mobile App</span>
+            <div className="footer__app--container">
+              <img src={AppleIcon} alt="app store" className="apple-icon" />
+              <img src={PlaystoreIcon} alt="google play"  className="playstore-icon"/>
             </div>
           </div>
         </div>
-        <div className="">
+        <div className="last-section">
           <p className="">© 2022 Weathery, All rights reserved to Team Gear</p>
           <div className="">
             <span className="">
